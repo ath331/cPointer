a5513c1a8a946cf049901432b7496c77e11241ba
ath331 <kstnftk@naver.com>
2020-02-03 22:06:25 +0900

    함수포인터 반환

--- C_Pointer/FuncPointer/FuncPoinetMain.cpp
+++ C_Pointer/FuncPointer/FuncPoinetMain.cpp
@@ -12,13 +12,27 @@ int sub(int numA, int numB)
 
 typedef int (*fptrOperator)(int, int);
 
-int compute(fptrOperator fptr, int num1, int num2)
+fptrOperator select(char a)
 {
-	return fptr(num1, num2);
+	switch (a)
+	{
+	case '+':
+			return add;
+	case '-':
+		return sub;
+	default:
+		break;
+	}
+}
+
+int evaluate(char opcode, int num1, int num2)
+{
+	fptrOperator operation = select(opcode);
+	return operation(num1, num2);
 }
 
 int main()
 {
-	std::cout << compute(add, 10, 20) << std::endl;
-	std::cout << compute(sub, 10, 20) << std::endl;
+	std::cout << evaluate('+', 10, 20) << std::endl;
+	std::cout << evaluate('-', 10, 20) << std::endl;
 }
