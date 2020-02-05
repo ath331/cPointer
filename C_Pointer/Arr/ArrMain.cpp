e3082a41deb51bb126ab231e124407cccfe23a43
ath331 <39950003+ath331@users.noreply.github.com>
2020-02-05 19:46:51 +0900

    2차원 배열 매개변수로 이용

--- C_Pointer/Arr/ArrMain.cpp
+++ C_Pointer/Arr/ArrMain.cpp
@@ -18,10 +18,25 @@ void DisplayArrPtr(int* arr, int size)
 	std::cout << std::endl;
 }
 
+void Display2DArrPtr(int arr[][5],int rows)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			std::cout << arr[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+}
+
 int main()
 {
-	int arr[5] = { 1, 2, 3, 5, 5 };
+	/*int arr[5] = { 1, 2, 3, 5, 5 };
 	int length = sizeof(arr) / sizeof(int);
 	DisplayArr(arr, length);
-	DisplayArrPtr(arr, length);
+	DisplayArrPtr(arr, length);*/
+
+	int arr2D[2][5] = { {1,2,3,4,5},{6,7,8,9,10} };
+	Display2DArrPtr(arr2D, 2);
 }
