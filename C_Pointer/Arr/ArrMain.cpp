5575deb5b646497a609b7331ca72f020aff5a7be
ath331 <39950003+ath331@users.noreply.github.com>
2020-02-06 21:47:03 +0900

    복합리터럴과 가변배열 생성
    c++에서는 안된다고 한다

--- C_Pointer/Arr/ArrMain.cpp
+++ C_Pointer/Arr/ArrMain.cpp
@@ -1,42 +1,47 @@
 #include <iostream>
+//
+//void DisplayArr(int arr[], int size)
+//{
+//	for (int i = 0; i < size; i++)
+//	{
+//		std::cout << arr[i] << " ";
+//	}
+//	std::cout << std::endl;
+//}
+//
+//void DisplayArrPtr(int* arr, int size)
+//{
+//	for (int i = 0; i < size; i++)
+//	{
+//		std::cout << *(arr + i) << " ";
+//	}
+//	std::cout << std::endl;
+//}
+//
+//void Display2DArrPtr(int arr[][5],int rows)
+//{
+//	for (int i = 0; i < rows; i++)
+//	{
+//		for (int j = 0; j < 5; j++)
+//		{
+//			std::cout << arr[i][j] << " ";
+//		}
+//		std::cout << std::endl;
+//	}
+//}
+//
+//int main()
+//{
+//	/*int arr[5] = { 1, 2, 3, 5, 5 };
+//	int length = sizeof(arr) / sizeof(int);
+//	DisplayArr(arr, length);
+//	DisplayArrPtr(arr, length);*/
+//
+//	int arr2D[2][5] = { {1,2,3,4,5},{6,7,8,9,10} };
+//	Display2DArrPtr(arr2D, 2);
+//}
 
-void DisplayArr(int arr[], int size)
+int main() //º¹ÇÕ ¸®ÅÍ·²
 {
-	for (int i = 0; i < size; i++)
-	{
-		std::cout << arr[i] << " ";
-	}
-	std::cout << std::endl;
-}
-
-void DisplayArrPtr(int* arr, int size)
-{
-	for (int i = 0; i < size; i++)
-	{
-		std::cout << *(arr + i) << " ";
-	}
-	std::cout << std::endl;
-}
-
-void Display2DArrPtr(int arr[][5],int rows)
-{
-	for (int i = 0; i < rows; i++)
-	{
-		for (int j = 0; j < 5; j++)
-		{
-			std::cout << arr[i][j] << " ";
-		}
-		std::cout << std::endl;
-	}
-}
-
-int main()
-{
-	/*int arr[5] = { 1, 2, 3, 5, 5 };
-	int length = sizeof(arr) / sizeof(int);
-	DisplayArr(arr, length);
-	DisplayArrPtr(arr, length);*/
-
-	int arr2D[2][5] = { {1,2,3,4,5},{6,7,8,9,10} };
-	Display2DArrPtr(arr2D, 2);
+	int(*(arr1[])) = { (int[]) {0,1,2} };
 }
